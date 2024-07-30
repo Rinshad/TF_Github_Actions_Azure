c5327d0da707ae3054dbe526bcb3ffb1afcacd46
rinshad <rinshadrabab@gmail.com>
2024-07-30 19:29:04 +0200

    terraform destroy added

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "mdp_prod_rg_1"
     storage_account_name = "prodtfstatefiles"
     container_name       = "terraformstatefiles"
-    key                  = "${var.prefix}.terraform.tfstate"
+    key                  = "github.terraform.tfstate"
   }
 }
