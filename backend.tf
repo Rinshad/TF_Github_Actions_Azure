7f77a6f70df89d2390ae14e7c7f7c5c087af478b
rinshad <rinshadrabab@gmail.com>
2024-07-30 16:55:29 +0200

    test

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "mdp_prod_rg_1"
     storage_account_name = "prodtfstatefiles"
     container_name       = "terraformstatefiles"
-    key                  = "github.terraform.tfstate"
+    key                  = "${var.prefix}.terraform.tfstate"
   }
 }
