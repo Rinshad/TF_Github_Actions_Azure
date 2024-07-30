f8dc15556240f552b6c97f19a4299bf27cced639
rinshad <rinshadrabab@gmail.com>
2024-07-30 20:00:01 +0200

    vnet added

---
+++ vnet.tf
@@ -0,0 +1,13 @@
+module "vnet" {
+  source  = "Azure/vnet/azurerm"
+  version = "3.0.0" # Specify the version of the module
+
+  # Define the resource group
+  resource_group_name = var.resource_group_name
+
+  # Define the VNet name and address space
+  vnet_name           = var.vnet_name
+  address_space       = var.address_space
+  location            = var.location
+  tags                = var.tags
+}
