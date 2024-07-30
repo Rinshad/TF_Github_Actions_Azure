a0ff392e886e0d23dd3ced684ebf6d98b6baadc4
rinshad <rinshadrabab@gmail.com>
2024-07-30 19:50:58 +0200

    vpc module created

--- variables.tf
+++ variables.tf
@@ -9,3 +9,28 @@ variable "container_name" {
   description = "The name of the Blob Storage container."
   default     = "my-terraform-state-container"
 }
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "The name of the Virtual Network"
+  type        = string
+}
+
+variable "address_space" {
+  description = "The address space for the Virtual Network"
+  type        = list(string)
+}
+
+variable "location" {
+  description = "The location/region where the VNet will be created"
+  type        = string
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
